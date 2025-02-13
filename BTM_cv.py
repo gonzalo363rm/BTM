c46804d399f234482ab556e751fc247e5771b52e
RMGonzalo <gonzalo363rm@gmail.com>
2025-02-13 18:23:10 -0300

    feat: cambio de package

---
+++ BTM_cv.py
@@ -0,0 +1,70 @@
+import bitermplus as btm
+import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords
+
+def preprocess_text(text): #recibe una tuit
+    spanish_stopwords = set(stopwords.words('spanish'))
+    tokens = text.split() #separamos en palabras
+
+    #filtramos las stopwords, las palabras cortas y las urls y volvemos a formar la oracion
+    return ' '.join([token for token in tokens if token not in spanish_stopwords and len(token) > 3 and 'twitter.com' not in token])
+
+# IMPORTING DATA
+# Cargar datos
+data = pd.read_csv("tweets_municipalidad.csv")
+texts = data['tweet'].astype(str)
+texts = [preprocess_text(text) for text in texts]
+
+# get spanish stop words
+spanish_stop_words = set(stopwords.words('spanish'))
+
+# PREPROCESSING
+# Obtaining terms frequency in a sparse matrix and corpus vocabulary
+X, vocabulary, vocab_dict = btm.get_words_freqs(texts, stop_words=list(spanish_stop_words))
+tf = np.array(X.sum(axis=0)).ravel()
+# Vectorizing documents
+docs_vec = btm.get_vectorized_docs(texts, vocabulary)
+docs_lens = list(map(len, docs_vec))
+# Generating biterms
+biterms = btm.get_biterms(docs_vec)
+
+parameters = {'T': [5, 10, 20, 30], 'M': [10, 20, 30], 'alpha': [0.01, 0.05, 0.1, 0.5, 3.3, 5.0], 'beta': [0.005, 0.01, 0.05, 0.1, 0.5], 'iterations': [5, 10, 25]}
+
+grid_search_results = []
+for T in parameters['T']:
+    for M in parameters['M']:
+        for alpha in parameters['alpha']:
+            for beta in parameters['beta']:
+                for iterations in parameters['iterations']:
+                    model = btm.BTM(X, vocabulary, seed=12321, T=T, M=M, alpha=alpha, beta=beta)
+                    model.fit(biterms, iterations=iterations)
+                    # coherence = btm.coherence(model.matrix_topics_words_, X, M=30)
+                    grid_search_results.append({'T': T, 'M': M, 'alpha': alpha, 'beta': beta, 'iterations': iterations, 'coherence': np.mean(model.coherence_)})
+
+# Encontrar los mejores hiperparámetros
+best_params = max(grid_search_results, key=lambda x: x['coherence'])
+print("Mejores parámetros encontrados:")
+print(best_params)
+
+
+# INITIALIZING AND RUNNING MODEL
+model = btm.BTM(
+    X, vocabulary, seed=12321, T=best_params['T'], M=best_params['M'], alpha=best_params['alpha'], beta=best_params['beta'])
+model.fit(biterms, iterations=best_params['iterations'])
+p_zd = model.transform(docs_vec)
+
+
+perplexity = model.perplexity_
+coherence = np.mean(model.coherence_)
+
+stable_topics = list(set(model.labels_))
+top_words = btm.get_top_topic_words(
+    model,
+    words_num=15,
+    topics_idx=stable_topics)
+
+print('top_words => ', top_words)
+print('coherence => ', coherence)
+print('perplexity => ', perplexity)
+
